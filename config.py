96c01a9c980651019f323a2262a07e2523050971
bloodybear <sincarisebastian@gmail.com>
2025-02-10 01:14:03 +0200

    model finished

--- config.py
+++
@@ -1,5 +0,0 @@
-BINANCE_URL = "https://api.binance.com/api/v3/klines"
-SYMBOL = "BTCUSDT"
-INTERVAL = "30m"
-LIMIT = 1000
-DAYS = 2600
--- dataset/save_data.py
+++
@@ -1,3 +0,0 @@
-def save_to_csv(df, filename):
-    df.to_csv(filename, index=False)
-    print(f"Data saved in {filename}")
